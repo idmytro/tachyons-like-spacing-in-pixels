8d864ef47ccadf58b82ceff9b2be25a2fd4a7a29
Dmytro Cisar <i@dmyt.ro>
2018-01-06 23:32:54 +0200

    + gulp-replace

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const replace = require('gulp-replace');
 
 gulp.task('sass', () => gulp.src('./scss/spacing-in-pixels.scss')
     .pipe(sass({
         /* Default: nested | Values: nested, expanded, compact, compressed */
         outputStyle: 'compact'
     }).on('error', sass.logError))
+    .pipe(replace(/\n\n/g, '\n'))
     .pipe(gulp.dest('./css/'))
 );
 
